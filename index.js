cf27ccce5c98d1a5d3342cc051d3ef64a1927fb6
liyonglong <liyonglong@xiaomi.com>
2018-11-09 10:56:27 +0800

    l

--- index.js
+++ index.js
@@ -49,12 +49,10 @@ module.exports = function (config) {
         if (!value) {
             value = routes[restPath];
         }
-        
-       
+            
         // pathParams匹配规则校验
         function isMatch(path, arr) {
             var units = path.split("/");
-
             return arr.some(function (rule) {
                 var temp = rule.split("/");
                 if (units.length === temp.length) {
@@ -134,44 +132,30 @@ module.exports = function (config) {
             }
             return params;
         }
-
+        
+        // 清除require缓存
         function deleteCache() {
-
             purgeCache(path.join(mockDir, entry))
         }
 
         function purgeCache(moduleName) {
-            // 遍历缓存来找到通过指定模块名载入的文件
             searchCache(moduleName, function (mod) {
                 delete require.cache[mod.id];
             });
-        
-            // 删除模块缓存的路径
-            // 多谢@bentael指出这点
-          Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
+            Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
                 if (cacheKey.indexOf(moduleName) > 0) {
                     delete module.constructor._pathCache[cacheKey];
                 }
             });
         };
         
-        /**
-         * 遍历缓存来查找通过特定模块名缓存下的模块
-         */
         function searchCache(moduleName, callback) {
-            //  通过指定的名字resolve模块
             var mod = require.resolve(moduleName);
-        
-            // 检查该模块在缓存中是否被resolved并且被发现
             if (mod && ((mod = require.cache[mod]) !== undefined)) {
-                // 递归的检查结果
                 (function traverse(mod) {
-                    // 检查该模块的子模块并遍历它们
                     mod.children.forEach(function (child) {
                         traverse(child);
                     });
-        
-                    // 调用指定的callback方法，并将缓存的module当做参数传入
                     callback(mod);
                 }(mod));
             }
